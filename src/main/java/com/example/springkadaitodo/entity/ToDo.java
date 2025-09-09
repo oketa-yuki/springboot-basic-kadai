d9540cd767a603e3fddbb35b28fa406b1bb2c01e
oketa-yuki <okechichi0627@gmail.com>
2025-09-09 17:18:04 +0900

    Add files via upload

---
+++ src/main/java/com/example/springkadaitodo/entity/ToDo.java
@@ -0,0 +1,38 @@
+package com.example.springkadaitodo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "todos")
+@Data
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Priority {
+        高, 中, 低
+    }
+
+    public enum Status {
+        未着手, 着手中, 完了
+    }
+}
